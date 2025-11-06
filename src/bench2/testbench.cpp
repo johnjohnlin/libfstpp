0f63a0d2f0dee5014d26b11080a7fcf41c82625e
Yoda Lee <lc85301@gmail.com>
2025-11-06 23:52:59 +0800

    Modify bench2 to add time measurement

--- src/bench2/testbench.cpp
+++ src/bench2/testbench.cpp
@@ -1,44 +1,61 @@
 #include "Vpicorv32_wrapper.h"
 #include "verilated_fst_c.h"
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono;
 
 int main(int argc, char **argv, char **env)
 {
-	printf("Built with %s %s.\n", Verilated::productName(), Verilated::productVersion());
-	printf("Recommended: Verilator 4.0 or later.\n");
+    auto tic = high_resolution_clock::now();
 
-	Verilated::commandArgs(argc, argv);
-	Vpicorv32_wrapper* top = new Vpicorv32_wrapper;
+    printf("Built with %s %s.\n", Verilated::productName(), Verilated::productVersion());
+    printf("Recommended: Verilator 4.0 or later.\n");
 
-	// Tracing (fst)
-	VerilatedFstC* tfp = NULL;
-	const char* flag_fst = Verilated::commandArgsPlusMatch("fst");
-	if (flag_fst && 0==strcmp(flag_fst, "+fst")) {
-		Verilated::traceEverOn(true);
-		tfp = new VerilatedFstC;
-		top->trace (tfp, 99);
-		tfp->open("testbench.fst");
-	}
+    // FST option parsing: last argument must be "1" or "0"
+    bool dump_fst = false;
+    if (argc > 1 && argv[argc-1][0] == '1') {
+		dump_fst = true;
+        --argc;
+    }
 
-	// Tracing (data bus, see showtrace.py)
-	FILE *trace_fd = NULL;
-	const char* flag_trace = Verilated::commandArgsPlusMatch("trace");
-	if (flag_trace && 0==strcmp(flag_trace, "+trace")) {
-		trace_fd = fopen("testbench.trace", "w");
-	}
+    Verilated::commandArgs(argc, argv);
+    Vpicorv32_wrapper* top = new Vpicorv32_wrapper;
 
-	top->clk = 0;
-	int t = 0;
-	while (!Verilated::gotFinish()) {
-		if (t > 200)
-			top->resetn = 1;
-		top->clk = !top->clk;
-		top->eval();
-		if (tfp) tfp->dump (t);
-		if (trace_fd && top->clk && top->trace_valid) fprintf(trace_fd, "%9.9lx\n", top->trace_data);
-		t += 5;
-	}
-	if (tfp) tfp->close();
-	delete top;
-	exit(0);
+    // Tracing (fst)
+    VerilatedFstC* tfp = NULL;
+    if (dump_fst) {
+        Verilated::traceEverOn(true);
+        tfp = new VerilatedFstC;
+        top->trace (tfp, 99);
+        tfp->open("testbench.fst");
+    }
+
+    // Tracing (data bus, see showtrace.py)
+    FILE *trace_fd = NULL;
+    const char* flag_trace = Verilated::commandArgsPlusMatch("trace");
+    if (flag_trace && 0==strcmp(flag_trace, "+trace")) {
+        trace_fd = fopen("testbench.trace", "w");
+    }
+
+    top->clk = 0;
+    int t = 0;
+    while (!Verilated::gotFinish()) {
+        if (t > 200)
+            top->resetn = 1;
+        top->clk = !top->clk;
+        top->eval();
+        if (tfp) tfp->dump (t);
+        if (trace_fd && top->clk && top->trace_valid) fprintf(trace_fd, "%9.9lx\n", top->trace_data);
+        t += 5;
+    }
+    if (tfp) tfp->close();
+    delete top;
+
+    auto toc = high_resolution_clock::now();
+    cout << duration_cast<microseconds>(toc-tic).count() << "us" << endl;
+
+    exit(0);
 }
 
