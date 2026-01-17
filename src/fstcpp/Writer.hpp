ff8f7063870a400d309095db8bbc37fdc6ceff95
Yu-Sheng Lin <johnjohnlys@gmail.com>
2026-01-17 18:06:11 +0800

    Update license file (#2)

--- src/fstcpp/Writer.hpp
+++ src/fstcpp/Writer.hpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2025-2026 Yu-Sheng Lin <johnjohnlys@gmail.com>
+// SPDX-FileCopyrightText: 2025-2026 Yoda Lee <lc85301@gmail.com>
+// SPDX-License-Identifier: MIT
 #pragma once
 // direct include
 #include "fstcpp/fst.hpp"
--- src/fstcpp/fst_string_view.hpp
+++ src/fstcpp/fst_string_view.hpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2025-2026 Yu-Sheng Lin <johnjohnlys@gmail.com>
+// SPDX-FileCopyrightText: 2025-2026 Yoda Lee <lc85301@gmail.com>
+// SPDX-License-Identifier: MIT
 #pragma once
 // Force to use nonstd::string_view
 #define nssv_CONFIG_SELECT_STRING_VIEW nssv_STRING_VIEW_NONSTD
